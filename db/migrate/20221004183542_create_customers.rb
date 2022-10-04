87b26e631d81c73bcf9c8bd0bccdce924fadec33
Lucas <lucas94gv@gmail.com>
2022-10-04 21:43:26 +0200

    [update] name customer required

--- db/migrate/20221004183542_create_customers.rb
+++ db/migrate/20221004183542_create_customers.rb
@@ -1,7 +1,7 @@
 class CreateCustomers < ActiveRecord::Migration[7.0]
   def change
     create_table :customers, id: :uuid do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
