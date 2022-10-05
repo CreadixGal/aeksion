06bcea0c137143c474ca34775295f28d52ccb2e8
ciscoLegrand <cisco.glez@gmail.com>
2022-10-05 22:48:22 +0200

    [test] add test results 📑

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
-  
+  get '/test-coverage', :to => redirect('/coverage/index.html')
 end
