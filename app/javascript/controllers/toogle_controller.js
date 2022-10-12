5288c1f228d3ac86ba05e39f337a150a9cba7334
ciscoLegrand <cisco.glez@gmail.com>
2022-10-12 20:39:53 +0200

    [style] aside responsive

--- app/javascript/controllers/toogle_controller.js
+++ app/javascript/controllers/toogle_controller.js
@@ -1,10 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "aside" ]
+  static targets = [ "aside", "button" ]
+
   hide(e) {
     e.preventDefault()
     const aside = this.asideTarget
+    const button = this.buttonTarget
     aside.classList.contains('hidden') ? aside.classList.remove('hidden') : aside.classList.add('hidden')
+    button.classList.contains('hidden') ? button.classList.remove('hidden') : button.classList.add('hidden')
   }
 }
