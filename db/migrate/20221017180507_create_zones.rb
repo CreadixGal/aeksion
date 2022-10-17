2c474314c50c675c5e1d628e68cabc6908712d20
Lucas <lucas94gv@gmail.com>
2022-10-17 20:55:29 +0200

    [fix] name in zones is not nullable

--- db/migrate/20221017180507_create_zones.rb
+++ db/migrate/20221017180507_create_zones.rb
@@ -1,7 +1,7 @@
 class CreateZones < ActiveRecord::Migration[7.0]
   def change
     create_table :zones, id: :uuid do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
