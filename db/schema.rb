39dc06e9304edb15ef3975e58d3d1d933689d18a
Lucas <lucas94gv@gmail.com>
2022-10-19 21:19:53 +0200

    [build] model product_movement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_190020) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_191453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -27,6 +27,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_190020) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["rate_id"], name: "index_movements_on_rate_id"
+  end
+
+  create_table "product_movements", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "movement_id", null: false
+    t.uuid "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movement_id"], name: "index_product_movements_on_movement_id"
+    t.index ["product_id"], name: "index_product_movements_on_product_id"
   end
 
   create_table "products", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -57,6 +67,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_190020) do
   end
 
   add_foreign_key "movements", "rates"
+  add_foreign_key "product_movements", "movements"
+  add_foreign_key "product_movements", "products"
   add_foreign_key "rates", "customers"
   add_foreign_key "rates", "zones"
 end
