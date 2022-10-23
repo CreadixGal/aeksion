a0fe250824543c047d8917ac604d3c57d7c0963d
ciscoLegrand <cisco.glez@gmail.com>
2022-10-23 11:59:18 +0200

    [style] add menu item

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  
   resources :customers do 
     collection do
       post :search
       delete :multiple_delete
     end
   end
-  root 'pages#index'
-  get 'dashboard', to: 'pages#dashboard', as: :dashboard
-  get '/test-coverage', :to => redirect('/coverage/index.html')
+  
+  get 'dashboard',      to: 'pages#dashboard', as: :dashboard
+  get 'configuration',  to: 'pages#configuration', as: :configuration
+  get '/test-coverage', to: redirect('/coverage/index.html')
 end
