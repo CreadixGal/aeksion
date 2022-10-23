13eab126606a3d94497bde3abf7471cb2231709d
ciscoLegrand <cisco.glez@gmail.com>
2022-10-23 13:32:11 +0200

    [build] configuration card component w/ specs ✅

---
+++ app/components/configuration_card_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class ConfigurationCardComponent < ViewComponent::Base
+  attr_reader :icon, :title, :description, :link_1, :link_2, :href_1, :href_2
+
+  def initialize(title:, description:, options: {})
+    super
+    @title = title
+    @description = description
+    @icon   = options[:icon].presence || 'info-circle.svg'
+    @link_1 = options[:link_1].presence || 'Consultar'
+    @link_2 = options[:link_2].presence || 'Crear'
+    @href_1 = options[:href_1]
+    @href_2 = options[:href_2]
+  end
+
+  def render?
+    title.present?
+  end
+end
---
+++ spec/components/configuration_card_component_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ConfigurationCardComponent, type: :component do
+  let(:required_params) do 
+    {
+      title: 'Clientes', 
+      description: 'Consulta, crea y edita clientes',
+      options: {
+        href_1: 'www.example.com',
+        href_2: 'www.test.com',
+      }
+    }
+  end
+
+  let(:full_params) do
+    {
+      icon: 'user-group.svg',
+      link_1: 'Ver clientes',
+      link_2: 'Crear cliente',
+    }
+  end
+
+  it 'renders default configuration card component' do
+    render_inline(described_class.new(title: required_params[:title], description: required_params[:description], options: required_params[:options]))
+
+    expect(page).to have_content required_params[:title]
+    expect(page).to have_content required_params[:description]
+    expect(page).to have_content 'Consultar'
+    expect(page).to have_link href: required_params[:options][:href_1]
+    expect(page).to have_content 'Crear'
+    expect(page).to have_link href: required_params[:options][:href_2]
+  end
+
+  it 'renders configuration card component with full params' do
+    options = required_params[:options].merge(full_params)
+    render_inline(described_class.new(title: required_params[:title], description: required_params[:description], options: options))
+
+    expect(page).to have_content required_params[:title]
+    expect(page).to have_content required_params[:description]
+    expect(page).to have_content 'Ver clientes'
+    expect(page).to have_link href: required_params[:options][:href_1]
+    expect(page).to have_content 'Crear cliente'
+    expect(page).to have_link href: required_params[:options][:href_2]
+  end
+end
