4d825452fea8246dc8b0e911787dc63ae3845d68
Lucas <lucas94gv@gmail.com>
2022-10-24 21:32:11 +0200

    [test] add tests to rates model

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -1,12 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Rate, type: :model do
-  subject { described_class.new(customer_id: 1, zone_id: 1, kind: 1, price: 0.5) }
+  subject do
 
-  it 'belongs' do
-    pending 'test incompleto'
-    expect(subject.customer_id).to belong_to(:customer)
+    @customer = Customer.create(name: "Juan")
+    @zone = Zone.create(name: "Galicia")
+
+    described_class.new(
+      customer_id: @customer,
+      zone_id: @zone,
+      kind: 1,
+      price: 0.5
+    )
   end
+
+  #it 'belongs' do
+  #  pending 'test incompleto'
+  #  expect(subject.customer_id).to belongs_to(:customer)
+  #end
+
+  #describe 'asliudbasibds' do
+  #it {should belong_to :customer}
+  #end
+
+  it { is_expected.to belong_to(:zone) }
+
+  
 
   it 'kind is integer' do
     expect(subject.kind).to be_a(Integer)
@@ -14,5 +33,21 @@ RSpec.describe Rate, type: :model do
 
   it 'price is float' do
     expect(subject.price).to be_a(Float)
+  end
+
+  it 'is Rate' do
+    expect(subject).to be_a(described_class)
+  end
+
+  it 'is not null' do
+    expect(subject).not_to be_nil
+  end
+
+  #it 'is valid with valid attributes' do
+  #  expect(subject).to be_valid
+  #end
+
+  it 'is a new rate and persisted' do
+    expect(subject).to be_a_new(described_class)
   end
 end
