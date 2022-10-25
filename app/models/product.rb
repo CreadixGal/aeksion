10382f50118963788b071ae1b9e00ddf61b3c4f1
ciscoLegrand <cisco.glez@gmail.com>
2022-10-25 22:46:53 +0200

    [build] add atachment and kind to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   has_one_attached :image
-  validates :code, presence: true
+
+  validates :code, :price, :stock, presence: true
+
+  enum :kind, { pale: 1, box: 2 }, field: { type: String, default: 'pale' }, map: :string, source: :kind
 end
