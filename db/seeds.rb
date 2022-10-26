fd60de9d7fd1cb8ef91b40aae69f365256aecdfb
ciscoLegrand <cisco.glez@gmail.com>
2022-10-26 22:35:54 +0200

    [fix] seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,18 @@
 file_name = 'pale.jpg'
 file_path = Rails.root.join('spec', 'factories', 'images', file_name)
 
-
 25.times do
-  product = Product.create(
+  product = Product.create!(
     code: SecureRandom.hex(8).to_s,
     price: rand(0.001..0.999),
     stock: rand(1..800),
     name: Faker::Commerce.product_name,
-    kind: [0, 1].sample
+    kind: [2, 1].sample
   )
 
   product.image.attach(io: File.open(file_path), filename: file_name, content_type: 'image/jpeg')
 end
 
 30.times do
-  Customer.create(name: Faker::Company.name)
+  Customer.create!(name: Faker::Company.name)
 end
