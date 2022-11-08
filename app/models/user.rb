c993626d96c2b5576b51f122c90646c2b0bcd88c
ciscoLegrand <cisco.glez@gmail.com>
2022-11-08 22:43:18 +0100

    [build] add custom validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
 
   validates :phone, :email, presence: true, uniqueness: true
   validates_format_of :email, with: Devise.email_regexp
-  validates :phone, format: { with: /\A[0-9]{9}\z/, message: 'only allows 9 digits' }
-  enum role: { superadmin: 0, admin: 1, user: 2 }, field: { type: Integer, default: 2 }, map: :string, source: :role
+  # validates :phone, format: { with: /\A[0-9]{9}\z/, message: 'only allows 9 digits' }
+  enum :role, { superadmin: 0, admin: 1, user: 2 }, field: { type: Integer, default: 2 }, map: :string, source: :role
+
+  def add_phone_prefix!
+    self.phone = "+34#{phone}" unless phone.start_with?('+34')
+    save!
+  end
 end
