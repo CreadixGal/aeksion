45dd8fc50fda39dac36e9293e0f81df149f60d48
ciscoLegrand <cisco.glez@gmail.com>
2022-11-09 23:01:56 +0100

    [build] add authentication_user! action on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, if: :devise_controller?
   include Pagy::Backend
+
   add_breadcrumb 'Inicio', :root_path
   add_flash_types :success, :error, :alert, :info, :notice
+
 end
