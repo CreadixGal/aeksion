f2980b2b79272917e8252c602849bf4a256ceb8e
ciscoLegrand <cisco.glez@gmail.com>
2022-11-19 11:18:13 +0100

    [refactor] add new model specs to zone for update new changes

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -3,4 +3,10 @@ class Zone < ApplicationRecord
 
   has_many :rates
   has_many :customers, through: :rates
+
+  validates :name, uniqueness: true
+
+  scope :ordered, -> { order(name: :desc) }
+
+  VALID_NAMES = %i[A_Coruña Lugo Ourense Pontevedra].freeze
 end
--- spec/models/zone_spec.rb
+++ spec/models/zone_spec.rb
@@ -1,34 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Zone, type: :model do
-  subject { described_class.new(name: Faker::Company.name) }
+  subject { described_class.new(name: Zone::VALID_NAMES.sample) }
 
-  it 'is string' do
-    expect(subject.name).to be_a(String)
+  context 'with valid attributes' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+
+    it 'is a new zone and persisted' do      
+      zone = described_class.create! name: 'random'
+      expect(zone).to be_persisted
+    end
   end
 
-  it 'is Zone' do
-    expect(subject).to be_a(described_class)
+  describe 'mandatory field' do
+    it 'name is string' do
+      expect(subject.name).to be_a(String)
+    end
+
+    it 'class is Zone' do
+      expect(subject).to be_a(Zone)
+    end
+
+    it 'class is not null' do
+      expect(subject).not_to be_nil
+    end
+    
+    it 'is not valid without name' do
+      subject.name = nil
+      expect(subject).not_to be_valid
+    end
+  
+    it 'name is not empty' do
+      expect(subject.name).not_to be_empty
+    end
   end
 
-  it 'is not null' do
-    expect(subject).not_to be_nil
-  end
-
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
-  end
-
-  it 'is not valid without name' do
-    subject.name = nil
-    expect(subject).not_to be_valid
-  end
-
-  it 'name is not empty' do
-    expect(subject.name).not_to be_empty
-  end
-
-  it 'is a new zone and persisted' do
-    expect(subject).to be_a_new(described_class)
+  describe 'show error message' do
+    it 'name can`t be blank' do
+      pending 'not show error message ❌'
+      subject.name = ''
+      subject.save!
+      expect(subject.errors[:name]).to include("can't be blank")
+    end
   end
 end
