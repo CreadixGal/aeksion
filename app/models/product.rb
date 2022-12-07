8c3d0363da1e01b08a67627e1ab6d40912c71cd3
ciscoLegrand <cisco.glez@gmail.com>
2022-12-07 21:32:30 +0100

    [build] add stock validation on products, greater than or equal to 0

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   has_many :movements, through: :product_movements, dependent: :destroy
 
   validates :code, :price, :stock, :kind, presence: true
+  validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'El stock no puede ser negativo' }
 
   enum :kind, { pallet: 1, box: 2 }, field: { type: Integer, default: 1 }, map: :string, source: :kind
 end
