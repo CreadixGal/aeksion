df0c4eafe14ec9297cc3a0293d4032927575c6b8
ciscoLegrand <cisco.glez@gmail.com>
2022-12-15 22:37:45 +0100

    [fix] notification component over all page elements

--- app/components/notification_component.rb
+++ app/components/notification_component.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class NotificationComponent < ViewComponent::Base
-  attr_reader :type, :data, :border_color
+  attr_reader :type, :data, :border_color, :stroke_color
 
   def initialize(type:, data:)
     super
     @type = type
     @data = prepare_data(data)
-    @border_color = color
+    @border_color = border_color
+    @stroke_color = stroke_color
     @data[:timeout] ||= 10
   end
 
@@ -22,18 +23,48 @@ class NotificationComponent < ViewComponent::Base
     end
   end
 
-  def color
+  def border_color
     case @type
     when 'success'
-      'border-b-green-400'
+      'border-t-green-400'
     when 'error'
-      'border-b-red-500'
+      'border-t-red-500'
     when 'alert'
-      'border-b-orange-500'
+      'border-t-orange-500'
     when 'info'
-      'border-b-indigo-400'
+      'border-t-indigo-400'
     else
-      'border-b-blue-400'
+      'border-t-blue-400'
+    end
+  end
+
+  def stroke_color
+    case @type
+    when 'success'
+      'stroke-green-400'
+    when 'error'
+      'stroke-red-500'
+    when 'alert'
+      'stroke-orange-500'
+    when 'info'
+      'stroke-indigo-400'
+    else
+      'border-t-blue-400'
+    end
+  end
+
+  def icon
+    case @type
+    when 'success'
+      'check-circle'
+    when 'error'
+      'exclamation-circle'
+    when 'alert'
+      'exclamation-triangle'
+    when 'info'
+      'info-circle'
+    else
+      'info-circle'
     end
   end
 end
