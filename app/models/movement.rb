92025ca84e718900a754244ede79eb1b46d9e5e8
ciscoLegrand <cisco.glez@gmail.com>
2022-12-19 21:41:04 +0100

    [build] method to auto generate a increment code on movement before create it

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -8,6 +8,19 @@ class Movement < ApplicationRecord
 
   accepts_nested_attributes_for :product_movements
 
+  before_create :run_code
+
   scope :delivery, -> { joins(:rate).where(rates: { kind: 'delivery' }) }
   scope :pickup, -> { joins(:rate).where(rates: { kind: 'pickup' }) }
+
+  def run_code
+    code = rate.delivery? ? Movement.delivery&.order(created_at: :asc).last&.code : Movement.pickup&.order(created_at: :asc).last&.code
+    code = code.nil? ? 0 : code
+    self.code = generate_code(code)
+  end
+
+  def generate_code(last_code)
+    code = last_code.to_s.slice(5, 7).to_i + 1
+    self.code = "#{rate.kind.slice(0..1).upcase}#{Time.now.year.to_s.last(2)}-#{code.to_s.rjust(7,'0')}"
+  end
 end
