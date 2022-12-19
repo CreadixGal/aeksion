31d33388b700a9cc6dc95bc1d29547c5f708b967
ciscoLegrand <cisco.glez@gmail.com>
2022-12-19 23:47:26 +0100

    [build] add movement validation, before_crete callbak to generete code

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -8,25 +8,27 @@ class Movement < ApplicationRecord
 
   accepts_nested_attributes_for :product_movements
 
-  after_create :run_code
+  before_create :run_code
+
+  validates :rate_id, :date, presence: true
+  validates :code, uniqueness: true
 
   scope :delivery, -> { joins(:rate).where(rates: { kind: 'delivery' }) }
   scope :pickup, -> { joins(:rate).where(rates: { kind: 'pickup' }) }
-
+  
   # rubocop:disable Lint/SafeNavigationChain
   def run_code
-    last_code = Movement.pickup&.order(created_at: :asc).last    if rate.pickup?
-    last_code = Movement.delivery&.order(created_at: :asc).last  if rate.delivery?
-    code = last_code.nil? ? 0 : last_code.code
+    code = Movement.delivery&.order(created_at: :asc).last&.code if rate.delivery?
+    code = Movement.pickup&.order(created_at: :asc).last&.code   if rate.pickup?
+    code = code.nil? ? 0 : code
     self.code = generate_code(code)
   end
   # rubocop:enable Lint/SafeNavigationChain
-
+  
   def generate_code(last_code)
-    code = last_code.to_s.slice(5, 7).to_i + 1
-    rate = self.rate.kind.slice(0..1).upcase
+    rate_code = rate.kind.slice(0..1).upcase
     year = Time.zone.now.year.to_s.last(2)
-    code = code.to_s.rjust(7, '0')
-    self.code = "#{rate}#{year}-#{code}"
+    code = last_code.to_s.slice(5, 7).to_i + 1
+    self.code = "#{rate_code}#{year}-#{code.to_s.rjust(7,'0')}"
   end
 end
