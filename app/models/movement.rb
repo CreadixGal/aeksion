a5789c24056983d7cd0d9c74d895362574d75c84
ciscoLegrand <cisco.glez@gmail.com>
2022-12-19 21:59:03 +0100

    [style] fix rubocop offenses

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -13,12 +13,14 @@ class Movement < ApplicationRecord
   scope :delivery, -> { joins(:rate).where(rates: { kind: 'delivery' }) }
   scope :pickup, -> { joins(:rate).where(rates: { kind: 'pickup' }) }
 
+  # rubocop:disable Lint/SafeNavigationChain
   def run_code
     last_code = Movement.pickup&.order(created_at: :asc).last    if rate.pickup?
     last_code = Movement.delivery&.order(created_at: :asc).last  if rate.delivery?
     code = last_code.nil? ? 0 : last_code.code
     self.code = generate_code(code)
   end
+  # rubocop:enable Lint/SafeNavigationChain
 
   def generate_code(last_code)
     code = last_code.to_s.slice(5, 7).to_i + 1
