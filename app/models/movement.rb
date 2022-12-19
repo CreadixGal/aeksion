47b2a298720c12f2eaa1e0b42665f3ff21558208
ciscoLegrand <cisco.glez@gmail.com>
2022-12-20 00:05:43 +0100

    [refactor] improve cyclomatic complexity of run code method

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -10,25 +10,33 @@ class Movement < ApplicationRecord
 
   before_create :run_code
 
-  validates :rate_id, :date, presence: true
+  validates :date, presence: true
   validates :code, uniqueness: true
 
   scope :delivery, -> { joins(:rate).where(rates: { kind: 'delivery' }) }
   scope :pickup, -> { joins(:rate).where(rates: { kind: 'pickup' }) }
-  
-  # rubocop:disable Lint/SafeNavigationChain
+
   def run_code
-    code = Movement.delivery&.order(created_at: :asc).last&.code if rate.delivery?
-    code = Movement.pickup&.order(created_at: :asc).last&.code   if rate.pickup?
-    code = code.nil? ? 0 : code
-    self.code = generate_code(code)
+    data = delivery_code if rate.delivery?
+    data = pickup_code   if rate.pickup?
+    data = 0 if data.nil?
+    self.code = generate_code(data)
+  end
+
+  # rubocop:disable Lint/SafeNavigationChain
+  def delivery_code
+    Movement.delivery&.order(created_at: :asc).last&.code
+  end
+
+  def pickup_code
+    Movement.pickup&.order(created_at: :asc).last&.code
   end
   # rubocop:enable Lint/SafeNavigationChain
-  
+
   def generate_code(last_code)
     rate_code = rate.kind.slice(0..1).upcase
     year = Time.zone.now.year.to_s.last(2)
-    code = last_code.to_s.slice(5, 7).to_i + 1
-    self.code = "#{rate_code}#{year}-#{code.to_s.rjust(7,'0')}"
+    increment = last_code.to_s.slice(5, 7).to_i + 1
+    "#{rate_code}#{year}-#{increment.to_s.rjust(7, '0')}"
   end
 end
