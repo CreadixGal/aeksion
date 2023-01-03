bff43d4f6e38957b5468f1fa7a2cbb663e637e29
ciscoLegrand <cisco.glez@gmail.com>
2023-01-03 22:23:00 +0100

    [build] add zerodivisionerror on rescue helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def circle_percentage(count_type, total)
-    (count_type * 100) / total
-  rescue
+    ((count_type * 100) / total)
+  rescue ZeroDivisionError
     0
   end
 end
