bea241c1d341e85debdbc3b6d19e6574aeea1866
ciscoLegrand <cisco.glez@gmail.com>
2023-01-03 22:22:30 +0100

    [build] filter movements by dates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,18 @@ class PagesController < ApplicationController
   def index; end
 
   def dashboard
-    @customers = Customer.all
+    @customers              = Customer.all
+    @total_movements        = Movement.all
+    @total_deliveries       = Movement.delivery
+    @total_pickups          = Movement.pickup
+    @last_month_movements   = Movement.where(date: 1.month.ago.all_month)
+    @last_month_deliveries  = Movement.delivery.where(date: 1.month.ago.all_month)
+    @last_month_pickups     = Movement.pickup.where(date: 1.month.ago.all_month)
+    @this_year_movements    = Movement.where(date: Time.zone.now.all_year)
+    @this_year_deliveries   = Movement.delivery.where(date: Time.zone.now.all_year)
+    @this_year_pickups      = Movement.pickup.where(date: Time.zone.now.all_year)
+    @this_week_movements    = Movement.where(date: Time.zone.now.all_week)
+    @this_week_deliveries   = Movement.delivery.where(date: Time.zone.now.all_week)
+    @this_week_pickup       = Movement.pickup.where(date: Time.zone.now.all_week)
   end
 end
