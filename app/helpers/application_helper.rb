53d8372de6f193b220f027c1f836ae27be28e6e9
ciscoLegrand <cisco.glez@gmail.com>
2023-01-04 19:43:27 +0100

    [refactor] improve readability from helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def circle_percentage(count_type, total)
-    ((count_type * 100) / total)
+    result = count_type.to_f * 100
+    result /= total.to_f
+    result
   rescue ZeroDivisionError
     0
   end
