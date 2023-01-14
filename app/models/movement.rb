857b618578149e4910a2f90109208c8a14271317
ciscoLegrand <cisco.glez@gmail.com>
2023-01-14 13:26:57 +0100

    [build] create a code generator service and add uses on validation_code method on movement model

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -15,11 +15,9 @@ class Movement < ApplicationRecord
   delegate :amount, to: :product_movements
   accepts_nested_attributes_for :product_movements
 
-  before_create :run_code
-
   validates :date, presence: true
-  validates :code, uniqueness: true
-
+  # validates :code, uniqueness: true
+  before_create :validate_code
   scope :delivery, -> { includes(%i[rate product_movements]).where(rates: { kind: 'delivery' }).order(date: :desc) }
   scope :pickup, -> { includes(%i[rate product_movements]).where(rates: { kind: 'pickup' }).order(date: :desc) }
   scope :sort_by_date, -> { order('date ASC') }
@@ -32,27 +30,7 @@ class Movement < ApplicationRecord
   #  customer: %i[name]
   # }
 
-  def run_code
-    data = delivery_code if rate.delivery?
-    data = pickup_code   if rate.pickup?
-    data = 0 if data.nil?
-    self.code = generate_code(data)
-  end
-
-  # rubocop:disable Lint/SafeNavigationChain
-  def delivery_code
-    Movement.delivery&.order(created_at: :asc).last&.code
-  end
-
-  def pickup_code
-    Movement.pickup&.order(created_at: :asc).last&.code
-  end
-  # rubocop:enable Lint/SafeNavigationChain
-
-  def generate_code(last_code)
-    rate_code = rate.kind.slice(0..1).upcase
-    year = Time.zone.now.year.to_s.last(2)
-    increment = last_code.to_s.slice(5, 7).to_i + 1
-    "#{rate_code}#{year}-#{increment.to_s.rjust(7, '0')}"
+  def validate_code
+    CodeGenerator.new(self).validate_code
   end
 end
---
+++ app/services/code_generator.rb
@@ -0,0 +1,38 @@
+class CodeGenerator
+  def initialize(resource)
+    @resource = resource
+    @code = @resource.code
+  end
+
+  def validate_code
+    duplicate = @resource.class.find_by(code: @resource.code).presence || false
+    @resource.errors.add(:code, 'is already taken') if duplicate
+    @resource.code = "#{@code}-dup" if duplicate
+  end
+
+  def run_code
+    return unless @resource.is_a? Movement
+
+    data = delivery_code if @resource.rate.delivery?
+    data = pickup_code   if @resource.rate.pickup?
+    data = 0 if data.nil?
+    @resource.code = generate_code(data)
+  end
+
+  # rubocop:disable Lint/SafeNavigationChain
+  def delivery_code
+    @resource.delivery&.order(created_at: :asc).last&.code
+  end
+
+  def pickup_code
+    @resource.pickup&.order(created_at: :asc).last&.code
+  end
+  # rubocop:enable Lint/SafeNavigationChain
+
+  def generate_code(last_code)
+    rate_code = @resource.rate.kind.slice(0..1).upcase
+    year = Time.zone.now.year.to_s.last(2)
+    increment = last_code.to_s.slice(5, 7).to_i + 1
+    "#{rate_code}#{year}-#{increment.to_s.rjust(7, '0')}"
+  end
+end
