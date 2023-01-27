c2520787ea41cf3a1895709aa4d4238b875643c4
ciscoLegrand <cisco.glez@gmail.com>
2023-01-27 16:12:49 +0100

    [refactor] avoid use of RailsException with better control of code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def circle_percentage(count_type, total)
+    return 0 if total.zero?
+
     result = count_type.to_f * 100
     result /= total.to_f
     result
-  rescue ZeroDivisionError
-    0
   end
 end
