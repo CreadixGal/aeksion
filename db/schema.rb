9d948971cd00673320e607a70da377d782678c36
Lucas <lucas94gv@gmail.com>
2023-02-02 17:06:52 +0100

    add field pickup_price for customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_200454) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_160456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_200454) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "pickup_price", precision: 8, scale: 4, default: "0.0", null: false
   end
 
   create_table "movements", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
