5d15aef26a5a479d7df8ca744ba41dd1f0fd531c
ciscoLegrand <cisco.glez@gmail.com>
2023-02-02 22:19:29 +0100

    [ui/ux] add notification status component on aside and navbar, create pending count method in applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
   add_breadcrumb 'Inicio', :root_path
   add_flash_types :success, :error, :alert, :info, :notice
+
+  before_action :set_pending_issue_count
+
+  private
+
+  def set_pending_issue_count
+    return 0 unless user_signed_in? && current_user.superadmin?
+
+    @pending_issues = IssueTracker.pending_count
+  end
 end
