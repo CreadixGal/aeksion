9430d8868a8f6f9a7e235cff429ddec315ab3b05
ciscoLegrand <cisco.glez@gmail.com>
2023-02-02 22:17:43 +0100

    [build] add status to issue tracker

--- app/models/issue_tracker.rb
+++ app/models/issue_tracker.rb
@@ -8,6 +8,13 @@ class IssueTracker < ApplicationRecord
               maximum: 100,
               message: 'must be between 10 and 100 characters'
             }
+  enum status: {
+    pending: 'pending',
+    viewed: 'viewed',
+    in_progress: 'in_progress',
+    completed: 'completed'
+  }, _default: 'pending'
 
   scope :ordered, -> { includes([images_attachments: [:blob]]).order(created_at: :desc) }
+  scope :pending_count, -> { where(status: :pending).count }
 end
--- db/migrate/20230131203509_create_issue_trackers.rb
+++ db/migrate/20230131203509_create_issue_trackers.rb
@@ -3,7 +3,7 @@ class CreateIssueTrackers < ActiveRecord::Migration[7.0]
     create_table :issue_trackers, id: :uuid do |t|
       t.string :title, null: false
       t.string :description, null: false
-
+      t.string :status, null: false, default: 'pending'
       t.timestamps
     end
   end
