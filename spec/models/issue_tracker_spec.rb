fa1a126dcea7e65b9b02d6ccaf8daa521d50d14e
ciscoLegrand <cisco.glez@gmail.com>
2023-03-12 13:40:14 +0100

    [refactor] issue tracker model spec

--- spec/models/issue_tracker_spec.rb
+++ spec/models/issue_tracker_spec.rb
@@ -1,42 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe IssueTracker do
-  subject { build(:issue_tracker, :with_image) }
+RSpec.describe IssueTracker, type: :model do
+  let(:issue) { build(:issue_tracker) }
 
-  describe 'with required fields' do
-    it 'must contain title' do
-      pending 'work in progress'
-      expect(subject.title).not_to be_empty
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
+    it { is_expected.to have_many_attached(:images) }
+    it { is_expected.to accept_nested_attributes_for(:comments) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+
+    it 'validates the length of the title' do
+      issue = described_class.new(title: 'a' * 9)
+      expect(issue).not_to be_valid
+
+      issue = described_class.new(title: 'a' * 101)
+      expect(issue).not_to be_valid
+
+      issue = described_class.new(title: nil)
+      expect(issue).not_to be_valid
+      expect(issue.errors[:title].first).to include("no puede estar en blanco")
     end
 
-    it 'title must be between 10 and 100 characters' do
-      pending 'work in progress'
-      expect(subject.title.length).to be_between(10, 100)
-    end
+    it { is_expected.to validate_presence_of(:comments) }
+  end
 
-    it 'title has validation error' do
-      pending 'work in progress'
-      subject.title = ''
-      expect(subject).not_to be_valid
-    end
-
-    it 'description has validation error' do
-      pending 'work in progress'
-      subject.description = ''
-      expect(subject).not_to be_valid
-    end
-
-    it 'title has a custom validation error message' do
-      pending 'work in progress'
-      subject.title = 'hello'
-      expect(subject).not_to be_valid
+  describe 'enums' do
+    it 'defines the status enum with the correct values and default' do
+      expect(described_class.statuses.keys).to eq(%w[pending viewed in_progress completed])
+      expect(issue.status).to eq('pending')
     end
   end
 
-  describe 'with optional file' do
-    it 'image is an ActiveStorage::Attached::Many' do
-      pending 'work in progress'
-      expect(subject.images).to be_a(ActiveStorage::Attached::Many)
+  describe 'scopes' do
+    describe '.ordered' do
+      it 'orders by created_at in descending order' do
+        issue1 = create(:issue_tracker, created_at: 1.day.ago)
+        issue2 = create(:issue_tracker)
+        issue3 = create(:issue_tracker, created_at: 2.days.ago)
+
+        expect(described_class.ordered).to eq [issue2, issue1, issue3]
+      end
+    end
+
+    describe '.pending_count' do
+      it 'returns the count of pending issues' do
+        create(:issue_tracker, status: :pending)
+        create(:issue_tracker, status: :viewed)
+
+        expect(described_class.pending_count).to eq 1
+      end
     end
   end
 end
