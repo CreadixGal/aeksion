4788bf78a222c55d887215d3151489e96dfaa348
ciscoLegrand <cisco.glez@gmail.com>
2023-03-12 12:58:33 +0100

    [refactor] comment and issue tracker factories

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    body { Faker::Lorem.paragraph(sentence_count: 3) }
-    user { create(:user) }
-    association :commentable, factory: :issue_tracker
+    user
+    body { Faker::Lorem.paragraphs(number: 10) }
   end
 end
--- spec/factories/issue_tracker.rb
+++ spec/factories/issue_tracker.rb
@@ -1,12 +1,17 @@
 FactoryBot.define do
   factory :issue_tracker do
     title { Faker::Lorem.sentence(word_count: 3) }
-    comments_attributes { [attributes_for(:comment)] }
+    user
+    status { 'pending' }
     after(:build) do |issue|
       file_name = 'issue.jpg'
       file_path = Rails.root.join('spec', 'factories', 'images', file_name)
       issue.images.attach(io: File.open(file_path),
                           filename: file_name, content_type: 'image/png')
+    end
+
+    after(:build) do |issue|
+      issue.comments << build(:comment)
     end
   end
 end
