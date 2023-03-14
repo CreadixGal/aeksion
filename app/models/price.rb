3a154afc04346b4820bbd8c204f210b77f9bb444
ciscoLegrand <cisco.glez@gmail.com>
2023-03-14 21:12:08 +0100

    [refactor] comment in prie model product_id validation

--- app/models/price.rb
+++ app/models/price.rb
@@ -2,10 +2,10 @@ class Price < ApplicationRecord
   belongs_to :priciable, polymorphic: true
 
   # rubocop:disable Rails/UniqueValidationWithoutIndex
-  validates :product_id,
-            uniqueness: {
-              scope: :priciable_id, if: :zone?
-            }
+  # validates :product_id,
+  #           uniqueness: {
+  #             scope: :priciable_id, if: :zone?
+  #           }
   # rubocop:enable Rails/UniqueValidationWithoutIndex
 
   private
