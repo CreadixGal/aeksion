ea94c978d4ff766ffdf39cc4b9241ec3a2a3fd97
ciscoLegrand <cisco.glez@gmail.com>
2023-03-14 21:13:49 +0100

    [refactor] remove price from validation, add foreign key to belongs_to zone,

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,16 @@
 class Product < ApplicationRecord
-  belongs_to :zone, optional: true
+  belongs_to :zone, optional: true, inverse_of: :products, foreign_key: :zone_id
 
   has_one_attached :image
 
+  has_many :prices, as: :priciable
   has_many :product_movements, inverse_of: :product, dependent: :destroy
   has_many :movements, through: :product_movements, dependent: :destroy
 
-  validates :code, :price, :stock, :kind, presence: true
+  validates :code, :stock, :kind, presence: true
   validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'El stock no puede ser negativo' }
 
   delegate :name, to: :zone, prefix: :zone
 
   enum :kind, { pallet: 1, box: 2 }, field: { type: Integer, default: 1 }, map: :string, source: :kind
-
-  has_one :price, as: :priciable
 end
