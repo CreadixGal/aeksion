2309ba97155c5a858c83a3e5c6be30d15250414e
ciscoLegrand <cisco.glez@gmail.com>
2023-03-18 13:25:00 +0100

    [refactor] remoce price code and zones from product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,9 @@
 class Product < ApplicationRecord
-  belongs_to :zone, optional: true, inverse_of: :products, foreign_key: :zone_id
-
   has_one_attached :image
 
-  has_many :prices, as: :priciable
   has_many :product_movements, inverse_of: :product, dependent: :destroy
   has_many :movements, through: :product_movements, dependent: :destroy
 
-  validates :code, :stock, :kind, presence: true
   validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'El stock no puede ser negativo' }
 
   delegate :name, to: :zone, prefix: :zone
