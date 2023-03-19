02d4c3529d5f4ada7058ae63c5fb10bb661a16b7
ciscoLegrand <cisco.glez@gmail.com>
2023-03-19 22:22:03 +0100

    [build] add nested zones method on tomselect controller

--- app/javascript/controllers/tomselect_controller.js
+++ app/javascript/controllers/tomselect_controller.js
@@ -2,11 +2,8 @@ import TomSelect from "tom-select"
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['product']
-  connect() {
-    this.nestedProducts()
-  }
-
+  static targets = ['product', 'zone']
+  connect() {}
 
   nestedProducts() {
     new TomSelect(this.productTarget, {
@@ -22,6 +19,21 @@ export default class extends Controller {
         return confirm(values.length > 1 ? 'Seguro que quiere quitar ' + values.length + ' items?' : 'Seguro que quieres quitar el producto?');
       }
     });
+  }
 
+  nestedZones() {
+    new TomSelect(this.zoneTarget, {
+      plugins: {
+        remove_button:{
+          title:'Remove this item',
+        }
+      },
+      placeholder: "Añadir",
+      hideSelected: true,
+      maxItems: 1,
+      onDelete: function(values) {
+        return confirm(values.length > 1 ? 'Seguro que quiere quitar ' + values.length + ' items?' : 'Seguro que quieres quitar la zona?');
+      }
+    });
   }
 }
