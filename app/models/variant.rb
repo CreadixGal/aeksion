3fbb0da540720833b6f13f9af296768060de6352
ciscoLegrand <cisco.glez@gmail.com>
2023-03-19 13:52:45 +0100

    [build] add delegations to zone on variant model

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -2,4 +2,6 @@ class Variant < ApplicationRecord
   belongs_to :product
   belongs_to :zone, inverse_of: :variants
   has_one :price, as: :priciable
+
+  delegate :name, to: :zone, prefix: :zone
 end
