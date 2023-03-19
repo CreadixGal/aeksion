6a7731a2478977281177e4a8575e012e5d30b41d
ciscoLegrand <cisco.glez@gmail.com>
2023-03-19 13:52:21 +0100

    [refactor] remove price_id from variants migration

--- db/migrate/20230318115307_create_variants.rb
+++ db/migrate/20230318115307_create_variants.rb
@@ -2,7 +2,6 @@ class CreateVariants < ActiveRecord::Migration[7.0]
   def change
     create_table :variants, id: :uuid do |t|
       t.references :product, null: false, foreign_key: true, type: :uuid
-      t.references :price, null: false, foreign_key: true, type: :uuid
       t.references :zone, null: false, foreign_key: true, type: :uuid
       t.string :code
 
