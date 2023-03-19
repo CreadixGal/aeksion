4ebeceef18f17d0d6e278327155c5853487f7576
ciscoLegrand <cisco.glez@gmail.com>
2023-03-19 22:21:27 +0100

    [refactor] product factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,2 @@
 FactoryBot.define do
-  factory :variant do
-    product { nil }
-    price { nil }
-    zone { nil }
-    code { "MyString" }
-  end
-
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
   factory :product do
-    code { "PR#{rand(0..999)}" }
-    kind { [1, 2].sample }
-    name { Faker::Commerce.product_name }
-    price { rand(0.001..0.999) }
+    name { "PR#{rand(0..999)}" }
+    kind { 1 }
     stock { rand(190..800) }
+
+    after(:create) do |product|
+      create(:variant, product:)
+    end
 
     trait :with_image do
       after :build do |product|
