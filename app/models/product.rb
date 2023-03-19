d6758029b612419d9d2c972904dbde7e10987e26
ciscoLegrand <cisco.glez@gmail.com>
2023-03-19 22:24:14 +0100

    [build] add nested attributes for variants on product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,13 @@
 class Product < ApplicationRecord
   has_one_attached :image
   has_many :variants, inverse_of: :product, dependent: :destroy
+  accepts_nested_attributes_for :variants, allow_destroy: true
 
   has_many :product_movements, inverse_of: :product, dependent: :destroy
   has_many :movements, through: :product_movements, dependent: :destroy
-  
+
   validates :stock, :kind, presence: true
   validates :stock, numericality: { greater_than_or_equal_to: 0, message: 'El stock no puede ser negativo' }
-
-  delegate :name, to: :zone, prefix: :zone
 
   enum :kind, { pallet: 1, box: 2 }, field: { type: Integer, default: 1 }, map: :string, source: :kind
 end
