8d99ea3f13d9e614ef8dc9e3fbc8b6392f961f96
ciscoLegrand <cisco.glez@gmail.com>
2023-03-24 19:54:56 +0100

    [build] dynamic nested tomselect form with variants by zones

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -7,6 +7,20 @@ export default class extends Controller {
     event.preventDefault()  
     var content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
     this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
+
+    const newSelect = this.element.querySelector(
+      "#select-variant:not([data-tomselect-initialized])"
+    );
+    if (newSelect) {
+      console.log("newSelect", newSelect)
+      newSelect.setAttribute("data-tomselect-initialized", "true");
+      const customEvent = new CustomEvent("nested-form:added", {
+        detail: { variantSelect: newSelect },
+      });
+      document.dispatchEvent(customEvent);
+    } else {
+      console.log("No new select found");
+    }
   }
 
   remove_association(event) {
