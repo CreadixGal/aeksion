5028fc6301fef6857fcf0b843c934cde6be53a50
ciscoLegrand <cisco.glez@gmail.com>
2023-03-28 22:21:21 +0200

    [build] create product rate with kind pickup before create customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,7 @@ class Customer < ApplicationRecord
   has_many :zones, through: :rates, dependent: :destroy
   has_one :price, as: :priciable
 
+  before_create :set_product_rate
   after_create :default_price
 
   delegate :quantity, to: :price, prefix: :price
@@ -15,5 +16,9 @@ class Customer < ApplicationRecord
 
   def default_price
     Price.create! quantity: 0, priciable: self
+  end
+
+  def set_product_rate
+    rates.build(zone: Zone.find_by(name: 'Product'), kind: 'pickup')
   end
 end
