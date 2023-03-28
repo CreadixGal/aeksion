c939e28f66a2896fd42a5604f420f77e60d97185
ciscoLegrand <cisco.glez@gmail.com>
2023-03-28 22:20:22 +0200

    [refactor] add error message on validations, update methods for calculate amount of product movements

--- app/models/product_movement.rb
+++ app/models/product_movement.rb
@@ -2,32 +2,42 @@ class ProductMovement < ApplicationRecord
   belongs_to :movement
   belongs_to :product
 
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity,
+            presence: {
+              message: "can't be blank"
+            },
+            numericality: {
+              greater_than: 0,
+              message: 'must be greater than 0'
+            }
 
   has_one :price, as: :priciable
+
+  validate :enough_stock, on: %i[create update]
+  after_create :calculate_stock
+  after_create :update_amount
 
   private
 
   def update_amount
-    calculate_amount(movement.rate.customer.price.quantity) if movement.rate.pickup?
-    calculate_amount(movement.rate.zone.price.quantity) if movement.rate.delivery?
+    calculate_amount(product.variants.find_by(zone_id: movement.rate.zone_id).quantity) if movement.rate.pickup?
+    calculate_amount(movement.rate.zone.quantity) if movement.rate.delivery?
   end
 
   def calculate_amount(price)
-    update! amount: price * quantity
+    update! amount: (price * self.quantity)
   end
-
-  # validate :enough_stock, on: %i[create update], if: -> { movement.rate.kind == 'delivery' }
-  after_create :calculate_stock
 
   def return!
     update!(return: true) unless return?
   end
 
   def enough_stock
+    return true if movement.rate_kind.eql?('pickup')
     return true if StockControl.new(self).enough_stock?
 
     errors.add(:quantity, 'no hay suficiente stock')
+    false
   end
 
   def calculate_stock
