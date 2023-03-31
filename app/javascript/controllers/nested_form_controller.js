fa39cc674ec6ba4353402cd6cb482368b1f6a47f
ciscoLegrand <cisco.glez@gmail.com>
2023-03-31 21:32:54 +0200

    [refactor] remove tomselect and use dynamic select for handling nested form and update data in the form

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -3,20 +3,34 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["add_item", "template"]
 
+  connect() {
+    document.addEventListener("nested-form:added", this.handleFormAdded.bind(this));
+  }
+
+  disconnect() {
+    document.removeEventListener("nested-form:added", this.handleFormAdded.bind(this));
+  }
+
+  handleFormAdded(event) {
+    // Actualizar el select de product_id aquí
+    // Puedes acceder al select recién agregado mediante event.detail.variantSelect
+  }
+
   add_association(event) {
-    event.preventDefault()  
+    event.preventDefault()
     var content = this.templateTarget.innerHTML.replace(/TEMPLATE_RECORD/g, new Date().valueOf())
     this.add_itemTarget.insertAdjacentHTML('beforebegin', content)
 
     const newSelect = this.element.querySelector(
-      "#select-variant:not([data-tomselect-initialized])"
+      "#select-variant:not([data-dynamic-select-initialized])"
     );
     if (newSelect) {
       console.log("newSelect", newSelect)
-      newSelect.setAttribute("data-tomselect-initialized", "true");
+      newSelect.setAttribute("data-dynamic-select-initialized", "true");
       const customEvent = new CustomEvent("nested-form:added", {
         detail: { variantSelect: newSelect },
       });
+      console.log('coas añadida')
       document.dispatchEvent(customEvent);
     } else {
       console.log("No new select found");
