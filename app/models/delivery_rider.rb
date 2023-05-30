e6f809022a613b639259f83968b04e54b5f00393
ciscoLegrand <cisco.glez@gmail.com>
2023-05-30 19:47:26 +0200

    [build] add has many movements through rates on delivery rider for searching method

--- app/models/delivery_rider.rb
+++ app/models/delivery_rider.rb
@@ -2,9 +2,9 @@ class DeliveryRider < ApplicationRecord
   validates :name, presence: true
 
   has_many :rates
+  has_many :movements, through: :rates
   has_many :zones, through: :rates, dependent: :destroy
   has_one :price, as: :priciable, dependent: :destroy
-
   # before_create :set_product_rate
   after_create :default_price
 
