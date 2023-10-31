8971c67af3dec880ef4e6a1e05b2bac175749728
ciscoLegrand <cisco.glez@gmail.com>
2023-10-31 08:58:19 +0100

    [feature] movement filters

--- app/components/date_searcher_component.rb
+++ app/components/date_searcher_component.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 class DateSearcherComponent < ViewComponent::Base
-  attr_reader :path, :placeholder, :kind, :regex, :zero, :name, :range
+  attr_reader :path, :placeholder, :kind, :regex, :zero, :name, :range, :product_kind
 
   def initialize(path:, options: {})
     super
     @path         = path
-    @placeholder  = options[:placeholder] || 'Search...'
-    @kind         = options[:kind]        || nil
-    @zero         = options[:zero]        || nil
-    @name         = options[:name]        || nil
-    @range        = options[:range]       || nil
+    @placeholder  = options[:placeholder]  || 'Search...'
+    @kind         = options[:kind]         || nil
+    @zero         = options[:zero]         || nil
+    @name         = options[:name]         || nil
+    @range        = options[:range]        || nil
+    @product_kind = options[:product_kind] || nil
   end
 end
