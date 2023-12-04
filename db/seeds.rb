24dcc9830fc7ee419d99494d81d3e4670e1b2075
Lucas-rzilient <lucas@rzilient.club>
2023-12-04 21:10:24 +0100

    update seeds with production data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,123 +1,137 @@
-require 'faker'
+if Rails.env.production? || Rails.env.staging?
 
-file_name = 'pale.jpg'
-file_path = Rails.root.join("spec/fixtures/files/#{file_name}")
+  User.first_or_create!(email: 'creadix@creadix.es', password: 'test123', role: 'superadmin')
+  User.first_or_create!(email: 'admin@orpal.es', password: 'orpal123', role: 'admin')
 
-Faker::Config.locale = :en
+  # create product zone
+  pr = Zone.first_or_create!(name: 'DEFAULT')
+  unless pr.price.present?
+    pr.price = Price.new(quantity: 0)
+    pr.save!
+  end
+else
 
-# create users (superadmin and admin)
-User.create!(email: 'sadmin@test.com', password: 'test123', role: 'superadmin')
-User.create!(email: 'admin@test.com', password: 'test123', role: 'admin')
-User.create!(email: 'creadix@creadix.es', password: 'test123', role: 'superadmin')
-User.create!(email: 'admin@orpal.es', password: 'orpal123', role: 'admin')
+  require 'faker'
 
-# create 4 zones
-co = Zone.create!(name: 'A Coruña')
-lu = Zone.create!(name: 'Lugo')
-ou = Zone.create!(name: 'Ourense')
-po = Zone.create!(name: 'Pontevedra')
-# create product zone
-pr = Zone.create!(name: 'DEFAULT')
-pr.price = Price.new(quantity: 0)
-pr.save!
+  file_name = 'pale.jpg'
+  file_path = Rails.root.join("spec/fixtures/files/#{file_name}")
 
-# add price to zones
-[co, lu, ou, po].each do |zone|
-  zone.price = Price.new(quantity: rand(0.001..0.999))
-  zone.save!
-end
+  Faker::Config.locale = :en
 
-# create 3 customers
-8.times do
-  c = Customer.create!(name: Faker::Company.name)
-  puts "\n🚚 Delivery rider  #{c.name} created 🚚\n"
-  dr = DeliveryRider.create!(name: Faker::Name.name)
-  puts "\n🚚 Delivery rider  #{dr.name} created 🚚\n"
-end
+  # create users (superadmin and admin)
+  User.create!(email: 'sadmin@test.com', password: 'test123', role: 'superadmin')
+  User.create!(email: 'admin@test.com', password: 'test123', role: 'admin')
+  User.create!(email: 'creadix@creadix.es', password: 'test123', role: 'superadmin')
+  User.create!(email: 'admin@orpal.es', password: 'orpal123', role: 'admin')
 
-# create rates
-DeliveryRider.all.each do |dr|
+  # create 4 zones
+  co = Zone.create!(name: 'A Coruña')
+  lu = Zone.create!(name: 'Lugo')
+  ou = Zone.create!(name: 'Ourense')
+  po = Zone.create!(name: 'Pontevedra')
+  # create product zone
+  pr = Zone.create!(name: 'DEFAULT')
+  pr.price = Price.new(quantity: 0)
+  pr.save!
+
+  # add price to zones
   [co, lu, ou, po].each do |zone|
-    del = zone.rates.build(
-      delivery_rider_id: dr.id,
-      kind: 'delivery'
+    zone.price = Price.new(quantity: rand(0.001..0.999))
+    zone.save!
+  end
+
+  # create 3 customers
+  8.times do
+    c = Customer.create!(name: Faker::Company.name)
+    puts "\n🚚 Delivery rider  #{c.name} created 🚚\n"
+    dr = DeliveryRider.create!(name: Faker::Name.name)
+    puts "\n🚚 Delivery rider  #{dr.name} created 🚚\n"
+  end
+
+  # create rates
+  DeliveryRider.all.each do |dr|
+    [co, lu, ou, po].each do |zone|
+      del = zone.rates.build(
+        delivery_rider_id: dr.id,
+        kind: 'delivery'
+      )
+
+      del.price = Price.new(quantity: rand(0.001..0.999))
+      del.save!
+      puts "\n📦 Delivery rate created    #{del} -> #{del.quantity}📦\n"
+    end
+  end
+
+  # create isolate single products
+  5.times do
+    code = rand(1000..9999)
+    product = Product.new(
+      name: "PR#{code}",
+      stock: rand(55_350..99_800),
+      kind: [2, 1].sample
     )
 
-    del.price = Price.new(quantity: rand(0.001..0.999))
-    del.save!
-    puts "\n📦 Delivery rate created    #{del} -> #{del.quantity}📦\n"
+    product.save!
+    puts "\n📦 Product #{product.name} created 📦\n"
+    Zone.where.not(name: 'DEFAULT').each do |zone|
+      variant_code = "PR#{code}-#{zone.name.downcase.tr('^a-z', '').slice(0, 2)}"
+      variant = product.variants.build(
+        code: variant_code,
+        zone_id: zone.id
+      )
+
+      variant.price = Price.new(quantity: rand(0.0001..0.9999))
+      variant.save!
+      puts "📦 Variant: #{variant.code} with price #{variant.quantity} created     📦\n"
+    end
   end
-end
 
-# create isolate single products
-5.times do
-  code = rand(1000..9999)
-  product = Product.new(
-    name: "PR#{code}",
-    stock: rand(55_350..99_800),
-    kind: [2, 1].sample
-  )
+  def create_delivery_product_movement(rate)
+    variant  = Variant.where(zone_id: rate.zone_id).sample
+    product  = variant.product
+    quantity = rand(1..100)
 
-  product.save!
-  puts "\n📦 Product #{product.name} created 📦\n"
-  Zone.where.not(name: 'DEFAULT').each do |zone|
-    variant_code = "PR#{code}-#{zone.name.downcase.tr('^a-z', '').slice(0, 2)}"
-    variant = product.variants.build(
-      code: variant_code,
-      zone_id: zone.id
-    )
-
-    variant.price = Price.new(quantity: rand(0.0001..0.9999))
-    variant.save!
-    puts "📦 Variant: #{variant.code} with price #{variant.quantity} created     📦\n"
+    { product_id: product.id, quantity: }
   end
+
+  # two_years_ago = 1.year.ago
+  # idx = 0
+  # while two_years_ago <= Time.zone.now
+  #   two_years_ago += 1.day
+  #   next if two_years_ago.saturday? || two_years_ago.sunday?
+
+  #   rand(3..9).times do
+  #     idx += 1
+  #     customer = Customer.all.sample
+  #     rate = customer.rates.where(kind: 'delivery').sample
+  #     puts kind = %w[delivery pickup].freeze.sample
+
+  #     product_movements = []
+  #     3.times do
+  #       if kind == 'delivery'
+  #         product_movement = create_delivery_product_movement(rate)
+  #         puts "🐛 #{product_movement} 🐛\n"
+  #         if (Product.find(product_movement[:product_id]).stock - product_movement[:quantity]).positive?
+  #           product_movements << product_movement
+  #         end
+  #       else
+  #         product_movements << { product_id: Product.all.sample.id, quantity: rand(100..700) }
+  #       end
+  #     end
+  #     puts "🐛 #{product_movements} 🐛\n"
+
+  #     mov = Movement.new(
+  #       code: "ALB-#{idx.to_s.rjust(4, '0')}",
+  #       rate_id: rate.id,
+  #       date: two_years_ago,
+  #       product_movements_attributes: product_movements
+  #     )
+
+  #     if mov.save
+  #       puts "📆 #{idx} #{mov.rate_kind}: #{two_years_ago}: #{mov.persisted?}\n"
+  #     else
+  #       puts "⚠️⚠️⚠️\n\nErrors #{mov.rate_kind}: #{mov.errors.inspect}\n\n⚠️⚠️⚠️"
+  #     end
+  #   end
+  # end
 end
-
-def create_delivery_product_movement(rate)
-  variant  = Variant.where(zone_id: rate.zone_id).sample
-  product  = variant.product
-  quantity = rand(1..100)
-
-  { product_id: product.id, quantity: }
-end
-
-# two_years_ago = 1.year.ago
-# idx = 0
-# while two_years_ago <= Time.zone.now
-#   two_years_ago += 1.day
-#   next if two_years_ago.saturday? || two_years_ago.sunday?
-
-#   rand(3..9).times do
-#     idx += 1
-#     customer = Customer.all.sample
-#     rate = customer.rates.where(kind: 'delivery').sample
-#     puts kind = %w[delivery pickup].freeze.sample
-
-#     product_movements = []
-#     3.times do
-#       if kind == 'delivery'
-#         product_movement = create_delivery_product_movement(rate)
-#         puts "🐛 #{product_movement} 🐛\n"
-#         if (Product.find(product_movement[:product_id]).stock - product_movement[:quantity]).positive?
-#           product_movements << product_movement
-#         end
-#       else
-#         product_movements << { product_id: Product.all.sample.id, quantity: rand(100..700) }
-#       end
-#     end
-#     puts "🐛 #{product_movements} 🐛\n"
-
-#     mov = Movement.new(
-#       code: "ALB-#{idx.to_s.rjust(4, '0')}",
-#       rate_id: rate.id,
-#       date: two_years_ago,
-#       product_movements_attributes: product_movements
-#     )
-
-#     if mov.save
-#       puts "📆 #{idx} #{mov.rate_kind}: #{two_years_ago}: #{mov.persisted?}\n"
-#     else
-#       puts "⚠️⚠️⚠️\n\nErrors #{mov.rate_kind}: #{mov.errors.inspect}\n\n⚠️⚠️⚠️"
-#     end
-#   end
-# end
