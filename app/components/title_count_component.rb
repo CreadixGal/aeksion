5c4c6adffedc1b9ef9f6920a452f8ef16deeb9e1
ciscoLegrand <cisco.glez@gmail.com>
2023-12-06 19:30:02 +0100

    Update TitleCountComponent class to include full width

--- app/components/title_count_component.rb
+++ app/components/title_count_component.rb
@@ -7,7 +7,7 @@ class TitleCountComponent < ViewComponent::Base
 
   def call
     content_tag :h1, id: 'count',
-                     class: 'font-bold px-12 py-2 flex flex-nowrap gap-4 my-12' do
+                     class: 'w-full font-bold px-12 py-2 flex flex-nowrap gap-4 my-12' do
       concat content_tag(:span, title_with_count, class: 'text-left text-black md:text-lg lg:text-xl xl:text-4xl')
     end
   end
